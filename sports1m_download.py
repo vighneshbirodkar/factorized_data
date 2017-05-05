97564877f8f30c46850a221e45cbf2694095cbc4
Vighnesh Birodkar <vighneshbirodkar@gmail.com>
2017-05-05 01:04:53 -0400

    random downloads after checking hash

--- sports1m_download.py
+++ sports1m_download.py
@@ -4,29 +4,45 @@ import argparse
 import pytube
 import subprocess
 import multiprocessing as mp
-import uuid
+import random
+import pprint
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--id_file', type=str, default='./train_partition.txt')
 parser.add_argument('--outdir', type=str, default='./output')
-parser.add_argument('--start_index', type=int, required=True)
-parser.add_argument('--end_index', type=int, required=True)
 parser.add_argument('--n_threads', type=int, default=8)
+parser.add_argument('--seed', type=int, default=None)
+
 
 opt = parser.parse_args()
+random.seed(opt.seed)
+
+pprint.pprint(vars(opt), indent=2)
 
 
 with open(opt.id_file) as f:
     video_urls = [line.split()[0] for line in f.readlines()]
 
 
-video_urls = video_urls[opt.start_index:opt.end_index]
+video_urls = video_urls
 preferred_resolutions = ['240p', '360p', '480p', '144p']
 
 
-def download_vid(url):
+def get_hash(url):
+    return hashlib.sha256(url.encode('utf-8')).hexdigest()[:10]
+
+
+def download_random_vid(_):
+
+    downloaded = set([name.split('.')[0] for name in os.listdir(opt.outdir)])
     
+    url = random.choice(video_urls)
+
+    while get_hash(url) in downloaded:
+        print('Skipping %s as %s exists' % (url, get_hash(url)))
+        url = random.choice(video_urls)
+
     try:
         yt = pytube.YouTube(url)
     except:
@@ -58,6 +74,6 @@ def download_vid(url):
 
 
 pool = mp.Pool(opt.n_threads)
-result = list(pool.map(download_vid, video_urls))
+result = list(pool.map(download_random_vid, range(10**6)))
 
 print('Downloaded %d videos' % sum(result))
